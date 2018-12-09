a552639a668babd692a12f05c8c6ddd097cb0058
SJ Seo <seungju1991@gmail.com>
2018-12-09 12:45:48 +0900

    edit hangman

--- games/hangman.py
+++ games/hangman.py
@@ -1,1 +1,25 @@
-#test hangman game
+import random
+life = 10
+
+print ("Hello! Lets play hangman!")
+name = input("Please write down your name : ")
+print("Hello, "+name)
+answer_list = ["apple","banana","cup","station","car","movie","coffee","telephone"]
+current_answer = answer_list[random.randint(0,len(answer_list))-1]
+print("current answer is "+str(current_answer))
+underbar = "_"
+start = underbar*len(current_answer)
+print("You have " +str(life)+ " chances")
+print("Here goes with the first question! Please answer the quiz below")
+print(start)
+x = input("please input your alphabet or answer : ")
+x == current_answer
+if len(x) == 1: #alphabet
+    print("x")
+else: #word
+    if x == current_answer:
+        print("correct!!!")
+    else:
+        print("wrong!!!")
+        life -= 1
+        print("You have " +str(life)+ " chances. Cheer up!")
